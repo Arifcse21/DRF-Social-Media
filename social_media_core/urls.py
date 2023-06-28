365ec6c984fe2f82f05186d4630604734166753d
Arifcse21 <arifcse21@gmail.com>
2023-06-28 19:52:20 +0600

    swagger and redoc configs added

--- social_media_core/urls.py
+++ social_media_core/urls.py
@@ -15,11 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Social Media API endpoints",
+        default_version='v0.1',
+        description="DRF Social Nerwork",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="arifcse21@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    validators=['ssv', 'flex'],
+    public=True,
+    permission_classes=[permissions.AllowAny,],
+)
+
+    
 urlpatterns = [
+    re_path('swagger(?P<format>.json|.yaml)$', schema_view.without_ui(cache_timeout=None), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('', include('sm_app.urls')),
 ]
