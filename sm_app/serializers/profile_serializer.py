3e81a46ff2b1f189b80eebde073a97abac91f355
Md Abdullah Al Arif <arifcse21@gmail.com>
2023-07-05 15:28:46 +0600

    User profile data update view and serializer added

--- sm_app/serializers/profile_serializer.py
+++ sm_app/serializers/profile_serializer.py
@@ -1,9 +1,43 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from sm_app.models import Profile
+from django.contrib.auth import get_user_model
 
-class ProfileSerializer(ModelSerializer):
 
+class ProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(max_length=50, trim_whitespace=False)
+    last_name = serializers.CharField(max_length=50, trim_whitespace=False)
+    
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = [
+            "first_name",
+            "last_name",
+            "pfp",
+            "bio",
+            "sm_links"
+        ]
         
+
+    def validate(self, attrs):
+        first_name = attrs.get('first_name')
+        last_name = attrs.get('last_name')
+        pfp = attrs.get('pfp')
+        bio = attrs.get('bio')
+        sm_links = attrs.get('sm_links')
+
+        if (first_name and last_name and pfp and bio and sm_links):
+            msg = "Required Fields: All fields data are required!"
+            raise serializers.ValidationError(msg, code="authorization")
+        else:
+            try:
+                uuid_from_url = self.context['view'].kwargs.get('uuid')
+                user = get_user_model().objects.get(uuid=uuid_from_url).update(
+                    first_name=first_name,
+                    last_name=last_name
+                )
+                user.save()
+            except Exception as e:
+                raise serializers.ValidationError(str(e), code="authorization")
+
+        return attrs
+    
--- sm_app/views/profile_update_view.py
+++ sm_app/views/profile_update_view.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from sm_app.serializers import ProfileSerializer
 from rest_framework.viewsets import ViewSet
 from drf_yasg.utils import swagger_auto_schema
+from django.contrib.auth import get_user_model
 
 
 class UpdateProfileView(ViewSet):
@@ -13,4 +14,29 @@ class UpdateProfileView(ViewSet):
         operation_description="This api registers new user and return access jw token"
     )
     def update(self, request, uuid):
-        pass 
+        
+        profile_data = request.data
+
+        try:
+            user = get_user_model().objects.filter(uuid=uuid)
+            if user.exists():
+                user = user.first()
+
+                serializer = ProfileSerializer(user, data=profile_data)
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+
+                    api_response = {
+                        "status": "successful",
+                        "message": "User profile updated",
+                        "data": serializer.data
+                    }
+                    return Response(api_response, status=status.HTTP_200_OK)
+                
+        except Exception as e:
+            api_response = {
+                "status": "failed",
+                "message": "User not found!",
+            }
+            return Response(api_response, status=status.HTTP_400_BAD_REQUEST)
+        
