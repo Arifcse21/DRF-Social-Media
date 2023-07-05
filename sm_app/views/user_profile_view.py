8b95d78f52fd43bcee5c9f8e637f8a9ace7c6c79
Md Abdullah Al Arif <arifcse21@gmail.com>
2023-07-05 18:30:52 +0600

    user profile view init

---
+++ sm_app/views/user_profile_view.py
@@ -0,0 +1,6 @@
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from rest_framework import status
+from sm_app.serializers import ProfileSerializer
+from sm_app.models import Profile
+
