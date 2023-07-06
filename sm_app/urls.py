e82fbf45c3d7d392350f82bb54b1ef7f01de1f96
Md Abdullah Al Arif <arifcse21@gmail.com>
2023-07-06 11:29:24 +0600

    urls updated

--- sm_app/urls.py
+++ sm_app/urls.py
@@ -3,11 +3,15 @@ from rest_framework.routers import DefaultRouter
 from sm_app.views import (
     RegisterUserView,
     LoginView,
+    UpdateProfileView,
+    UserProfileView
 )
 router = DefaultRouter()
 
 router.register('register', RegisterUserView, basename="register-user")
 router.register('login', LoginView, basename='login-user')
+router.register('profile', UserProfileView, basename='user-profile')
+router.register('profile/update', UpdateProfileView, basename='update-profile')
 
 
 
