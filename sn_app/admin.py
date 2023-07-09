861bb10f271138fe180ec25f387e137201217745
Arifcse21 <arifcse21@gmail.com>
2023-07-09 14:03:50 +0600

    admin.py updated

--- sn_app/admin.py
+++ sn_app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from sn_app.models import User, Profile
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "username",
+        "is_active",
+        "is_staff",
+        "is_superuser"
+    ]
+
+    list_display_links = ["username",]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "created_at",
+        "modified_at"
+    ]
+
+    list_display_links = ["user",]
