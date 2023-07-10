3df73701931332c1cbcf571a484ef72f336510b4
Md Abdullah Al Arif <arifcse21@gmail.com>
2023-07-10 14:09:44 +0600

    post creation api created

--- sn_app/models/post_model.py
+++ sn_app/models/post_model.py
@@ -4,13 +4,15 @@ from django.contrib.auth import get_user_model
 
 class Post(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=250, unique=True, null=True, blank=True)
+    slug = models.SlugField(max_length=250, unique=True, null=True, blank=True)
     content = models.TextField()
-    upvote = models.PositiveBigIntegerField(null=True, blank=True)
-    downvote = models.PositiveBigIntegerField(null=True, blank=True)
-
+    upvote = models.PositiveIntegerField(null=True, blank=True)
+    downvote = models.PositiveIntegerField(null=True, blank=True)
+    is_archieved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     modified_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self) -> str:
-        return f"{self.user}: {self.id}"
+        return f"{self.user}: {self.slug}"
     
---
+++ sn_app/views/create_post_view.py
@@ -0,0 +1,56 @@
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from sn_app.serializers import PostSerializer
+from sn_app.models import Post, User
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from sn_app.utils import (SafeJWTAuthentication, decode_uuid_from_jwt)
+# from sn_app.decorators import JWTRequired
+
+
+class CreatePostView(ViewSet):
+    authentication_classes = [SafeJWTAuthentication, ]
+
+    # @JWTRequired
+    @swagger_auto_schema(
+        request_body=PostSerializer,
+        operation_summary="Create a new post",
+        operation_description="This api let a valid user create new post",
+        responses={
+            "201": "Created a new post",
+            "400": "Cannot create new post"
+        }
+    )
+    def create(self, request):
+        uuid = decode_uuid_from_jwt(request.headers["Authorization"])
+
+        post_data = request.data
+
+
+        try:
+            payload = {
+            "user": get_object_or_404(User, uuid=uuid),
+            "title": post_data["title"],
+            "content": post_data["content"],
+            }
+
+            serializer = PostSerializer(data=payload)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            api_response = {
+                "status": "successful",
+                "message": f"{request.user} created a new post",
+                "data": serializer.data
+            }
+            return Response(api_response, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            api_response = {
+                "status": "successful",
+                "message": str(e)
+            }
+            return Response(api_response, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
